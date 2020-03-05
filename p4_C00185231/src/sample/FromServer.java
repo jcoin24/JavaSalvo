754a7f3f8dd21bdeab624cbf652fdfc78b1bfac0
ethanMoody <54563236+ethanMoody@users.noreply.github.com>
2020-03-04 20:08:43 -0600

    adds files I forgot (#5)

--- p4_C00185231/src/sample/FromServer.java
+++ p4_C00185231/src/sample/FromServer.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class FromServer {
-}
+
+import java.io.*;
+import java.net.*;
+
+public class FromServer implements Runnable {
+
+  Socket sock;
+  BufferedReader in = null;
+
+  public FromServer(Socket sock)throws Exception {
+    this.sock = sock;
+    in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+  }
+
+  public void run() {
+    String input;
+    try {
+      while ((input=in.readLine()) != null) {
+        System.out.println(input);
+      }
+    } catch (Exception e) {
+      System.out.println("Exception: " + e);
+    }
+    finally {
+      try {
+        in.close();
+      } catch (Exception e) {
+        System.out.println("Exception: " + e);
+      }
+    }
+  }
+
+} // end of class FromServer
